84365d0f446a7572405d4fc13a9ac52afae42801
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-10-09 22:26:11 -0300

    refactor:Change to Deploy

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express, { json } from "express";
 import "express-async-errors";
+import cors from "cors";
 import errorHandler from "@/middlewares/errorHandler";
 import router from "@/routers/index.routes";
 
 const app = express();
-app
-    .use(json())
-    .use(router)
-    .use(errorHandler);
+
+app.use(json()).use(cors()).use(router).use(errorHandler);
 
 export default app;
--- tests/helpers.ts
+++
@@ -1,6 +0,0 @@
-import prisma from "../src/database/db.connection";
-
-export async function cleanDB() {
-  await prisma.profile.deleteMany();
-  await prisma.user.deleteMany();
-}
